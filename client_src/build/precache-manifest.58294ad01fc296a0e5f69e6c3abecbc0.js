b3ec041bf8b4a2dc0a1a4733969465621bc063a3
unknown <shawnchen1980@hotmail.com>
2019-02-21 01:01:34 +0800

    add frontend files

---
+++ client_src/build/precache-manifest.58294ad01fc296a0e5f69e6c3abecbc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cc4a4f7344f4568e3c2",
+    "url": "/static/js/main.6cc4a4f7.chunk.js"
+  },
+  {
+    "revision": "7d3846bd5cc36ddaf1d0",
+    "url": "/static/js/1.7d3846bd.chunk.js"
+  },
+  {
+    "revision": "6cc4a4f7344f4568e3c2",
+    "url": "/static/css/main.b377e943.chunk.css"
+  },
+  {
+    "revision": "a2d72e8292b7e2079a67c7e426481d7a",
+    "url": "/index.html"
+  }
+];
