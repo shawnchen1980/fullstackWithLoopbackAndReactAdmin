7604b36579635e9f44545c8b0c71675b825d25b7
shawn <shawnchen1980@hotmail.com>
2019-03-05 23:05:05 +0800

    server side: 1 fix the hasmanythrough relation by adding keythrough property in both word.json and wordbook.json 2 in custom.js, use Wordbook.scope to define a remote method and an endpoint for wordbook model, in the afterRemote hook, change the return result to the included word list

--- server/boot/custom.js
+++ server/boot/custom.js
@@ -1,17 +1,37 @@
 module.exports = function(app) {
-
-    function modifyResponse(ctx, model, next) {
+  function returnWordsForSayHello(ctx, model, next) {
     //   var status = ctx.res.statusCode;
     //   if (status && status === 200) {
     //     status = 201;
     //   }
     //   ctx.res.set('Content-Location', 'the internet');
     //   ctx.res.status(status).end();
-      console.log("ctx.result",ctx.result,ctx.methodString);
-      //ctx.res.set('X-Total-Count',10);
+    console.log("ctx.result", ctx.result, ctx.methodString);
+    if (Array.isArray(ctx.result)) {
+      console.log("ctx.result is array");
+      ctx.result[0].words.find().then(val => {
+        ctx.result = val;
+        next();
+      });
+      //ctx.result = ctx.result[0].words;
+      // console.log(ctx.result[0].words.map(x => x.toJSON()));
+      // if (Array.isArray(ctx.result[0].words)) {
+      //   ctx.result = ctx.result[0].words;
+      // }
+    }
+    //ctx.res.set('X-Total-Count',10);
+    else {
       next();
     }
-  
-    app.models.Wordbook.afterRemote('**', modifyResponse);
-    //app.models.ModelTwo.afterRemote('**', modifyResponse);
-  };
+  }
+
+  app.models.Wordbook.afterRemote("__get__sayhello", returnWordsForSayHello);
+  //app.models.ModelTwo.afterRemote('**', modifyResponse);
+  //reference from this page
+  //https://loopback.io/doc/en/lb3/Include-filter.html
+  var Wordbook = app.models.Wordbook;
+  Wordbook.scope("sayhello", {
+    include: "words",
+    where: { name: "new book1" }
+  });
+};
